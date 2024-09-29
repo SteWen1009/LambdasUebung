6441f9c11ea9cc56c5ae15df59c5455eea947224
Stefan Wenzlhumer <stefan.wenzlhumer@gmail.com>
2024-09-29 18:50:15 +0200

    completed NumberTest interface

---
+++ LambdasUebung/src/net/htlgkr/dreib/wenst/LambdaUebung/HalloJavaMitForEach.java
@@ -0,0 +1,24 @@
+package net.htlgkr.dreib.wenst.LambdaUebung;
+
+import java.util.Arrays;
+import java.util.List;
+
+class HalloJavaMitForEach {
+    public void run() {
+        System.out.println("=== Beispiel 1: HalloJavaMitForEach ===");
+
+        List<String> liste = Arrays.asList("Hallo", "Java", "mit", "forEach");
+
+        //alte Variante
+        for (String s : liste) {
+            System.out.println(s);
+        }
+
+        // mit ForEach-Methode
+        liste.forEach((String s) -> System.out.println(s));
+
+        // noch kuerzer
+        liste.forEach(System.out::println);
+    }
+}
+
---
+++ LambdasUebung/src/net/htlgkr/dreib/wenst/LambdaUebung/NumberTest.java
@@ -0,0 +1,5 @@
+package net.htlgkr.dreib.wenst.LambdaUebung;
+
+public interface NumberTest {
+    boolean testNumber(int number);
+}
